a6fa3a78d851bade641594430ae0b55318b32ab8
Hunter Stich <hunterstich@gmail.com>
2018-04-04 08:55:44 -0700

    added Query interface

--- src/main/kotlin/admin/firestore/CollectionReference.kt
+++ src/main/kotlin/admin/firestore/CollectionReference.kt
@@ -6,12 +6,10 @@ import kotlin.js.Promise
  * A CollectionReference object can be used for adding documents, getting document references, and querying for documents (using the methods inherited from Query).
  * @see <a href="https://cloud.google.com/nodejs/docs/reference/firestore/0.10.x/CollectionReference">CollectionReference</a>
  */
-external interface CollectionReference {
+external interface CollectionReference: Query {
     val firestore: Firestore
     val id: String
     val parent: DocumentReference?
     fun add(data: DocumentData): Promise<DocumentData>
     fun doc(documentPath: String): DocumentReference
-    fun where(fieldPath: String, opStr: String, value: Any)
-    fun get(): Promise<QuerySnapshot>
 }
